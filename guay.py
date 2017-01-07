8d29bfd8bb481bdf5598b4022cbf317fb73d0bfb
Diego <dmarquetal@gmail.com>
2017-01-07 09:24:39 +0100

    cambiado

---
+++ guay.py
@@ -0,0 +1,9 @@
+# esto lo ha hecho dmarquetal
+lista_mis_deportes_favoritos = ["baloncesto", "balonmano", "tenis", "padel", "futbol"]
+# print(lista_mis_deportes_favoritos)
+
+for deporte in lista_mis_deportes_favoritos:
+    if deporte == "futbol":
+        print "futbol puagggg"
+    else:
+        print "me gusta el ", deporte
---
+++ nueva.py
@@ -0,0 +1,7 @@
+listad = ["baloncesto", "balonmano", "padel",]
+listaf = ["arroz", "ensaladilla", "chuletillas",]
+listaa = ["Marcos", "Asier", "Ochoa",]
+listafa = ["tato", "papi", "mami", "y" "demas",]
+listatodo = ["todo" "esto" "es" "lo" "que" "mas" "aprecio", "bueno" "la" "comida" "es" "que" "es" "necesaria"]
+milista = [listad, listaf, listaa, listafa, listatodo]
+print(milista).append("bieen")
